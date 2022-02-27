e19ec5a4bd4086efd15ce06db597776c8cd313d8
Alexey Kudryavtsev <axel_47@mail.ru>
2022-02-27 18:31:03 +0300

    First commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+package WorkingWithNumbers;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> intList2 = new ArrayList<>();
+        for (Integer item : intList) {
+            if (item > 0 && (item % 2 == 0)) {
+                intList2.add(item);
+            }
+        }
+        Collections.sort(intList2);
+        System.out.println(intList2);
+        }
+    }
+
